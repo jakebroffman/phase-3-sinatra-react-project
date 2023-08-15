f8641c2d2acc101387c62391f9f41b92ee6a09bc
jakebroffman <jake.broffman1088@gmail.com>
2023-08-15 15:00:18 -0400

    refactor code for seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,20 @@ puts "🌱 Seeding spices..."
 
 
 
-Aisle.create(aisle_number: 1, aisle_category: "Dairy")
-Aisle.create(aisle_number: 2, aisle_category: "Produce")
-Aisle.create(aisle_number: 3, aisle_category: "Bread/Rolls")
-Aisle.create(aisle_number: 4, aisle_category: "Spices/Condiments")
-Aisle.create(aisle_number: 5, aisle_category: "Deli")
+
+a1 = Aisle.create(aisle_number: 1, aisle_category: "Dairy")
+a2 = Aisle.create(aisle_number: 2, aisle_category: "Produce")
+a3 = Aisle.create(aisle_number: 3, aisle_category: "Bread/Rolls")
 
 
 
-Product.create(name: "Ham", price: 4, aisle_id: 5)
-Product.create(name: "Roast Beef", price: 5, aisle_id: 5)
-Product.create(name: "Sour Cream", price: 3, aisle_id: 1)
-Product.create(name: "Heavy Cream", price: 6, aisle_id: 1)
-Product.create(name: "Foccacia", price: 3, aisle_id: 3)
-Product.create(name: "Wheat Bread", price: 4, aisle_id: 3)
-Product.create(name: "Salt", price: 1, aisle_id: 4)
-Product.create(name: "Cayenne Pepper", price: 2, aisle_id: 4)
-Product.create(name: "Romaine", price: 4, aisle_id: 2)
-Product.create(name: "Red Onion", price: 2, aisle_id: 2)
-Product.create(name: "Russett Potato", price: 1, aisle_id: 2)
+a1.products.create(name: "Milk", price: 4)
+a1.products.create(name: "Sour Cream", price: 3)
+a3.products.create(name: "Foccacia", price: 3)
+a3.products.create(name: "Wheat Bread", price: 4)
+a2.products.create(name: "Romaine", price: 4)
+a2.products.create(name: "Red Onion", price: 2)
+a2.products.create(name: "Russett Potato", price: 1)
 
 
 
