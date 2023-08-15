b21dacdc8538920db856248c4f5ad9995a320a82
jakebroffman <jake.broffman1088@gmail.com>
2023-08-15 12:22:49 -0400

    write seed data for Aisles and Products tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+
+
+Aisle.create(aisle_number: 1, aisle_category: "Dairy")
+Aisle.create(aisle_number: 2, aisle_category: "Produce")
+Aisle.create(aisle_number: 3, aisle_category: "Bread/Rolls")
+Aisle.create(aisle_number: 4, aisle_category: "Spices/Condiments")
+Aisle.create(aisle_number: 5, aisle_category: "Deli")
+
+
+
+Product.create(name: "Ham", price: 4, aisle_id: 5)
+Product.create(name: "Roast Beef", price: 5, aisle_id: 5)
+Product.create(name: "Sour Cream", price: 3, aisle_id: 1)
+Product.create(name: "Heavy Cream", price: 6, aisle_id: 1)
+Product.create(name: "Foccacia", price: 3, aisle_id: 3)
+Product.create(name: "Wheat Bread", price: 4, aisle_id: 3)
+Product.create(name: "Salt", price: 1, aisle_id: 4)
+Product.create(name: "Cayenne Pepper", price: 2, aisle_id: 4)
+Product.create(name: "Romaine", price: 4, aisle_id: 2)
+Product.create(name: "Red Onion", price: 2, aisle_id: 2)
+Product.create(name: "Russett Potato", price: 1, aisle_id: 2)
+
+
+
 
 puts "✅ Done seeding!"
