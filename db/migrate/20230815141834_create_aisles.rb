f24bb10e2b44dd632248ca4f5ab75f49ba099a06
jakebroffman <jake.broffman1088@gmail.com>
2023-08-15 10:22:22 -0400

    write create_table method in CreateAisles migration

--- db/migrate/20230815141834_create_aisles.rb
+++ db/migrate/20230815141834_create_aisles.rb
@@ -1,4 +1,9 @@
 class CreateAisles < ActiveRecord::Migration[6.1]
   def change
+    create_table :aisles do |t|
+      t.integer :aisle_number
+      t.string :aisle_category
+      t.timestamps
+    end
   end
 end
